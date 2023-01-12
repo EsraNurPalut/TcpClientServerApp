44accddc337c59acc562648975219e5a390dea7b
EsraNurPalut <nuresrapalut@outlook.com>
2023-01-13 00:15:35 +0300

    Add project files.

---
+++ SLClientApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace SLClientApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Net.Sockets.TcpClient clientSocket = new System.Net.Sockets.TcpClient();
+            clientSocket.Connect("127.0.0.1", 600);
+            while (true)
+            {
+                NetworkStream serverStream = clientSocket.GetStream();
+                byte[] outStream = System.Text.Encoding.ASCII.GetBytes(Console.ReadLine());
+                serverStream.Write(outStream, 0, outStream.Length);
+                serverStream.Flush();
+            }
+        }
+    }
+}
---
+++ SLServerApp/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace SLServerApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPAddress ıPAddress = IPAddress.Parse("127.0.0.1");
+            TcpListener serverSocket = new TcpListener(ıPAddress, 600);
+            TcpClient clientSocket = default(TcpClient);
+            serverSocket.Start();
+            Console.WriteLine(" *****SERVER HAZIR******");
+            Console.WriteLine("Client Bekleniyor...");
+            clientSocket = serverSocket.AcceptTcpClient();
+            Console.WriteLine("Client ile bağlantı sağlandı....");
+
+            while (true)
+            {
+                try
+                {
+                    NetworkStream networkStream = clientSocket.GetStream();
+                    byte[] bytesFrom = new byte[1024];
+                    if (networkStream.DataAvailable)
+                    {
+                        networkStream.Read(bytesFrom, 0, bytesFrom.Length);
+                        string dataFromClient = System.Text.Encoding.ASCII.GetString(bytesFrom);
+                        dataFromClient = dataFromClient.Substring(0, dataFromClient.IndexOf('\0'));
+                        Console.WriteLine("Gelen kutusu :" + dataFromClient);
+                        Console.WriteLine(DateTime.Now);
+                        networkStream.Flush();
+
+                    }
+
+                }
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine(ex.ToString());
+                }
+            }
+        }
+    }
+}
